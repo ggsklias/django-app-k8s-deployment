0b0f4350d777ea32fc4e32d7afd007e8d03a5830
megivh13 <ggsklias@gmail.com>
2025-03-06 04:27:56 +0100

    both test using AWS secrets manager in python for the db secret key

--- djangoarticleapp/djangoarticleapp/settings.py
+++ djangoarticleapp/djangoarticleapp/settings.py
@@ -24,11 +24,43 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Use this code snippet in your app.
+# If you need more information about configurations
+# or implementing the sample code, visit the AWS docs:
+# https://aws.amazon.com/developer/language/python/
+
+import boto3
+from botocore.exceptions import ClientError
+
+def get_secret():
+
+    secret_name = "prod/djangoApp/Postgres"
+    region_name = "eu-central-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    return get_secret_value_response['SecretString']
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m9r44pdjsnah9r@k&4u*82=i*@i65*92tmni_-%bb*zt5r1!tp'
+SECRET_KEY = get_secret()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
