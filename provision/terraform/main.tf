694abec13cd3909849e8453c745a2c74ba6ee250
megivh13 <ggsklias@gmail.com>
2025-03-19 15:11:43 +0100

    both add 80 port security group

--- provision/terraform/main.tf
+++ provision/terraform/main.tf
@@ -206,6 +206,14 @@ resource "aws_security_group" "allow_ssh_and_k8s" {
   }
 
   ingress {
+    description = "Allow HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = -1
     to_port     = -1
     protocol    = "icmp"
