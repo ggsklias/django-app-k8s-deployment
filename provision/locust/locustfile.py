2b47222dadb3f474ca5843aa1fc0dcb2a16dbf45
megivh13 <ggsklias@gmail.com>
2025-03-23 13:28:25 +0100

    all - update locust

--- provision/locust/locustfile.py
+++ provision/locust/locustfile.py
@@ -1,12 +1,15 @@
 from locust import HttpUser, task, between
 
 class WebsiteUser(HttpUser):
-    wait_time = between(1, 3)  # Simulates think time between requests
+    wait_time = between(1, 3)
+    host = "http://your-app-domain.com"
 
-    @task(2)
-    def view_homepage(self):
-        self.client.get("/")
+    def on_start(self):
+        # Perform login and store cookies if needed
+        response = self.client.post("/login", {"username": "your_username", "password": "your_password"})
+        if response.status_code != 200:
+            print("Login failed!")
 
-    @task(1)
-    def view_article(self):
-        self.client.get("/articles/1")  # Replace with a valid slug/id
+    @task
+    def view_articles(self):
+        self.client.get("/articles/")
