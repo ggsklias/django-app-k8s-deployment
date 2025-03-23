23e0ef0b08c4dbb5c3782429a9a164ee8026561d
megivh13 <ggsklias@gmail.com>
2025-03-23 13:32:39 +0100

    all fix host issue

--- provision/locust/locustfile.py
+++ provision/locust/locustfile.py
@@ -1,8 +1,9 @@
 from locust import HttpUser, task, between
+import os
 
 class WebsiteUser(HttpUser):
     wait_time = between(1, 3)
-    host = "http://your-app-domain.com"
+    host = os.getenv("LOCUST_TARGET_HOST")
 
     def on_start(self):
         # Perform login and store cookies if needed
@@ -13,3 +14,9 @@ class WebsiteUser(HttpUser):
     @task
     def view_articles(self):
         self.client.get("/articles/")
+        self.client.get("/")
+    
+    @task
+    def create_articles(self):
+        self.client.get("/create_article/")
+
