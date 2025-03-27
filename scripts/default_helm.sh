d695b1fa27aa606fc254f298547bf8ce6ae9c104
megivh13 <ggsklias@gmail.com>
2025-03-27 17:47:32 +0100

    all - remove path specific stuff

--- scripts/default_helm.sh
+++ scripts/default_helm.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+ls -la
 curl -L https://get.helm.sh/helm-v3.9.0-linux-x86_64.tar.gz -o helm.tar.gz
+ls -la
 tar -zxvf helm.tar.gz
+ls -la
 mv linux-amd64/helm /usr/local/bin/helm
 chmod +x /usr/local/bin/helm
 helm version --short
--- scripts/default_terraform.sh
+++ scripts/default_terraform.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-aws s3 cp s3://$AWS_BUCKET_NAME/terraform/terraform.tfstate .
-aws s3 cp s3://$AWS_BUCKET_NAME/terraform/.terraform.lock.hcl .
+aws s3 cp s3://$AWS_BUCKET_NAME/terraform/terraform.tfstate provision/terraform
+aws s3 cp s3://$AWS_BUCKET_NAME/terraform/.terraform.lock.hcl provision/terraform
 terraform -chdir=provision/terraform init
